c2582b0c86b8cdf423fc5530c1fedd3adb4ed129
Shreyash Thengne <105958364+ShreyashThengne@users.noreply.github.com>
2024-02-20 09:31:54 +0530

    Time: 643 ms (94.65%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,4 +1,4 @@
 -- Write your PostgreSQL query statement below
-SELECT e2.unique_id, e1.name
-FROM Employees as e1 left JOIN EmployeeUNI as e2
-on e1.id = e2.id;
+SELECT EmployeeUNI.unique_id, Employees.name
+FROM Employees left JOIN EmployeeUNI
+on Employees.id = EmployeeUNI.id;
