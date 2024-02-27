c83d2eb242e8aecb9197cb7d29c9c5675895c8cd
Shreyash Thengne <105958364+ShreyashThengne@users.noreply.github.com>
2024-02-28 01:00:40 +0530

    Time: 1992 ms (5.08%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,11 @@
+-- Write your PostgreSQL query statement below
+SELECT q1.person_name
+FROM queue q1
+cross join
+queue q2
+where q2.turn <= q1.turn
+-- order by q1.turn, q2.turn
+group by q1.person_id, q1.person_name, q1.turn
+having sum(q2.weight) <= 1000
+order by q1.turn DESC
+limit 1
