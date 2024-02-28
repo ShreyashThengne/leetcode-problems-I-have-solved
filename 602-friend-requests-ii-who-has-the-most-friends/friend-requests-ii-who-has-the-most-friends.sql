05de6c79e989c61725b48d4934721d564203ef15
Shreyash Thengne <105958364+ShreyashThengne@users.noreply.github.com>
2024-02-28 11:58:17 +0530

    Time: 565 ms (23.25%) | Memory: 0B (100.00%) - LeetSync

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -1,18 +1,4 @@
 -- Write your PostgreSQL query statement below
--- SELECT requester_id as id, count as num
--- FROM (
---     SELECT requester_id, count FROM (
---         SELECT requester_id, count(accepter_id) + (
---             SELECT count(*) from RequestAccepted r2 
---             where r2.accepter_id = r1.requester_id
---             )
---             as count
---         FROM RequestAccepted r1
---         group by requester_id
---     )
---     order by count DESC
---     limit 1
--- )
 SELECT id, count(id) as num
 FROM (
     (SELECT requester_id as id FROM RequestAccepted)
